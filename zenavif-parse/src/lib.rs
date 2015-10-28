imazen/zenavif

commit 32510efa726ce4b643a634efeac03429e115b840

Improve documentation.
Add a module-level doctest for the capi module, and improve
the description of the Error enum.

zenavif-parse/src/lib.rs
@@ -1,8 +1,8 @@
-// Module for parsing ISO Base Media Format aka video/mp4 streams.
+//! Module for parsing ISO Base Media Format aka video/mp4 streams.
 
 // This Source Code Form is subject to the terms of the Mozilla Public
 // License, v. 2.0. If a copy of the MPL was not distributed with this
-// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+// file, You can obtain one at https:mozilla.org/MPL/2.0/.
 
 extern crate byteorder;
 use byteorder::ReadBytesExt;
@@ -12,18 +12,22 @@ use std::io::Cursor;
 use std::cmp;
 use std::fmt;
 
-/// Expose C api wrapper.
+// Expose C api wrapper.
 pub mod capi;
 // FIXME: We can 'pub use capi::*' in rustc 1.5 and later.
 pub use capi::{mp4parse_new, mp4parse_free, mp4parse_read};
 
+/// Describes parser failures.
+///
+/// This enum wraps athe standard `io::Error` type, unified with
+/// our own parser error states and those of crates we use.
 #[derive(Debug)]
 pub enum Error {
     /// Custom error type for reporting parse errors.
     InvalidData,
-    /// Reflect byteorder::Error::UnexpectedEOF for short data.
+    /// Reflect `byteorder::Error::UnexpectedEOF` for short data.
     UnexpectedEOF,
-    /// Propagate underlying errors from std::io.
+    /// Propagate underlying errors from `std::io`.
     Io(std::io::Error),
 }
 

zenavif-parse/src/capi.rs
@@ -1,5 +1,22 @@
-// C API for mp4parse module.
-// Parses ISO Base Media Format aka video/mp4 streams.
+//! C API for mp4parse module.
+//!
+//! Parses ISO Base Media Format aka video/mp4 streams.
+//!
+//! # Examples
+//!
+//! ```rust
+//! extern crate mp4parse;
+//!
+//! // Minimal valid mp4 containing no tracks.
+//! let data = b"\0\0\0\x0cftypmp42";
+//!
+//! let context = mp4parse::mp4parse_new();
+//! unsafe {
+//!     let rv = mp4parse::mp4parse_read(context, data.as_ptr(), data.len());
+//!     assert_eq!(0, rv);
+//!     mp4parse::mp4parse_free(context);
+//! }
+//! ```
 
 // This Source Code Form is subject to the terms of the Mozilla Public
 // License, v. 2.0. If a copy of the MPL was not distributed with this
@@ -30,7 +47,11 @@ pub unsafe extern "C" fn mp4parse_free(context: *mut MediaContext) {
     let _: Box<MediaContext> = std::mem::transmute(context);
 }
 
-/// Feed a buffer through read_box(), returning the number of detected tracks.
+/// Feed a buffer through `read_box()` with the given rust-side
+/// parser context, returning the number of detected tracks.
+///
+/// This is safe to call with NULL arguments but will crash
+/// if given invalid pointers, as is usual for C.
 #[no_mangle]
 pub unsafe extern "C" fn mp4parse_read(context: *mut MediaContext, buffer: *const u8, size: usize) -> i32 {
     // Validate arguments from C.

commit e333dcf8aa11b3ca236c6f65f7ee665a49ef42a3

Fix MPL url.
I accidentally broke this in 32510efa726c. Thanks to kinetik
for catching it.

zenavif-parse/src/lib.rs
@@ -2,7 +2,7 @@
 
 // This Source Code Form is subject to the terms of the Mozilla Public
 // License, v. 2.0. If a copy of the MPL was not distributed with this
-// file, You can obtain one at https:mozilla.org/MPL/2.0/.
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
 extern crate byteorder;
 use byteorder::ReadBytesExt;